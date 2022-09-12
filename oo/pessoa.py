f111532ed0c6264bb6f4d324c4031736783af79a
robson <robsonlnx@gmail.com>
2022-09-12 20:31:50 -0300

    criada atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __int__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Robson'
+    print(p.nome)
+    print(p.idade)
