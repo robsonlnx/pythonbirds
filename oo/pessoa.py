27c97ec99485f17a26141f6e3c8f236b5f7a1037
robson <robsonlnx@gmail.com>
2022-09-12 22:54:19 -0300

    adicionado exercicio carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,53 @@
-class Pessoa:
-    def __int__(self, nome=None, idade=35):
-        self.idade = idade
-        self.nome = nome
 
-    def cumprimentar(self):
-        return f'Olá {id(self)}'
+
+from __future__ import annotations
+from typing import List
+
+
+class Pessoa:
+    olhos = 2
+
+    def __init__(self, *filhos: Pessoa, nome: str = None, idade=35):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
+    def cumprimentar(self) -> str:
+        return f'Ola {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_class(cls):
+        return f'{cls.olhos} - olhos'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Robson'
-    print(p.nome)
-    print(p.idade)
+    robson = Pessoa(nome='Robson')
+    luciano = Pessoa(robson, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+
+    for filho in luciano.filhos:
+        print(filho.nome)
+
+    luciano.sobrenome = 'Ramalho'
+
+    del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
+    print(luciano.__dict__)
+    print(robson.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(robson.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(robson.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_class(), luciano.nome_e_atributos_de_class())
+
